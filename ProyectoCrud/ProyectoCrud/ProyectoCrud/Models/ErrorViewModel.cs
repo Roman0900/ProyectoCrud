e30afb0c08f884caa1309acf5aa3209bef01aaad
Federico Pereyra <crearcuen15@gmail.com>
2022-08-19 02:27:00 -0300

    first commit

---
+++ ProyectoCrud/ProyectoCrud/ProyectoCrud/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoCrud.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
